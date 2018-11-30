ada4b5f9d61d51453e0864240a56d48cd3c6e36c
Sanoj Nambiar <knambiar@homeaway.com>
2018-11-30 17:46:08 -0600

    Initial commit

---
+++ HelloNerd/HelloNerd/HelloNerd.h
@@ -0,0 +1,19 @@
+//
+//  HelloNerd.h
+//  HelloNerd
+//
+//  Created by Kutty Sanoj Nambiar on 11/30/18.
+//  Copyright © 2018 HomeAway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HelloNerd.
+FOUNDATION_EXPORT double HelloNerdVersionNumber;
+
+//! Project version string for HelloNerd.
+FOUNDATION_EXPORT const unsigned char HelloNerdVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloNerd/PublicHeader.h>
+
+
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# ExcerciseGithubReleaseHook
+# TestFastLaneReleaseAsset
+
+
+## Requirements
+
+* Xcode `10.0` (download [here](https://developer.apple.com/download/))
+* Swift `4.2` (installed via Xcode)
+* Carthage `0.29` (installed via [Homebrew](https://brew.sh/))
+* Fastlane (installed via [Homebrew](https://brew.sh/))
